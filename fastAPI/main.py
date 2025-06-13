95b8d5b791fac0858cbbf319fa9ae1668427e19c
AJ Brownson <92381518+AJBrownson@users.noreply.github.com>
2025-06-13 12:02:07 +0100

    Update main.py

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -12,7 +12,7 @@ def read_root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://rfmodel.netlify.app/"],
+    allow_origins=["http://localhost:5173", "https://rfmodel.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
